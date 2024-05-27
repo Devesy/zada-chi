24f2abf30904576974b2649d091a901bc32fab52
Anton Kalinin <trakses@gmail.com>
2024-05-27 16:51:39 +0300

    Create solve.rb
    https://leetcode.com/problems/search-a-2d-matrix/

---
+++ solve.rb
@@ -0,0 +1,19 @@
+# @param {Integer[][]} matrix
+# @param {Integer} target
+# @return {Boolean}
+def search_matrix(matrix, target)
+    if matrix.length == 1 && matrix[0].length == 1
+        return matrix[0][0] == target
+    end
+
+    rows0 = matrix.map { |r| r[0] }
+    target_row = -1
+    (0..(rows0.length - 1)).each do |i|
+        if rows0[i] <= target && ((i == rows0.length - 1) || target <= rows0[i + 1])
+            target_row = i
+        end
+    end
+    return false if target_row == -1
+
+    matrix[target_row].include? target
+end
