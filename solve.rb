0857b8c9e4b515a81163c50eef5232b93280544f
Anton Kalinin <trakses@gmail.com>
2024-05-27 17:04:24 +0300

    Update solve.rb
    + test cases

--- solve.rb
+++ solve.rb
@@ -17,3 +17,6 @@ def search_matrix(matrix, target)
 
     matrix[target_row].include? target
 end
+
+puts search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
+puts search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)
