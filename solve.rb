6bdf5f2b2d2ad5714de43f34b39dfe7073cfced1
Anton Kalinin <trakses@gmail.com>
2024-05-30 22:37:26 +0400

    Update solve.rb
    https://leetcode.com/problems/set-matrix-zeroes/description/

--- solve.rb
+++ solve.rb
@@ -1,1 +1,27 @@
+# @param {Integer[][]} matrix
+# @return {Void} Do not return anything, modify matrix in-place instead.
+def set_zeroes(matrix)
+    rows = []
+    cols = []
+    matrix.each_with_index do |row, i|
+        row.each_with_index do |col, j|
+            if row[j] == 0
+                rows << i
+                cols << j
+            end
+        end
+    end
+    rows.uniq!
+    cols.uniq!
 
+    rows.each do |r|
+        matrix[r].each_with_index do |col, j|
+            matrix[r][j] = 0
+        end
+    end
+    cols.each do |c|
+        matrix.each_with_index do |row, i|
+            matrix[i][c] = 0
+        end
+    end
+end
