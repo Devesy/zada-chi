ac296addc511b8c1b90a407f0d6a0fb9ce6db6b8
Anton Kalinin <trakses@gmail.com>
2024-06-04 01:26:10 +0400

    Update solve.rb
    https://leetcode.com/problems/zigzag-conversion/

--- solve.rb
+++ solve.rb
@@ -1,1 +1,27 @@
+# @param {String} s
+# @param {Integer} num_rows
+# @return {String}
+def convert(s, num_rows)
+    return s if num_rows == 1
 
+    m = Array.new(num_rows, '')
+    m.each_with_index { |r, i| m[i] = Array.new(s.length, '') }
+
+    x = 0
+    y = 0
+    chars = s.each_char.to_a.each_slice((2 * num_rows - 2) > 0 ? (2 * num_rows - 2) : 1 ).with_index do |slice, j|
+        slice.each_with_index do |c, i|
+            # pp "#{x}, #{y}, #{c}"
+            m[x][y] = c
+            if i < num_rows - 1
+                x += 1
+            else
+                x -= 1
+                y += 1
+            end
+        end
+        y += ((j + 1) * num_rows - 1)
+    end
+    # pp m.map(&:join).join
+    m.map(&:join).join
+end
