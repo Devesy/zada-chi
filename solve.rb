0cfef6ae841d42728b311105aa15ade7e516ef70
Anton Kalinin <trakses@gmail.com>
2024-06-05 16:39:38 +0400

    Update solve.rb
    https://leetcode.com/problems/top-k-frequent-elements/

--- solve.rb
+++ solve.rb
@@ -1,1 +1,10 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Integer[]}
+def top_k_frequent(nums, k)
+    return nums if nums.length <= k
 
+    freqs = Hash.new(0)
+    nums.each { |n| freqs[n] += 1 }
+    freqs.to_a.sort { |a, b| a[1] < b[1] ? 1 : -1 }.map { |a| a[0] }.take(k)
+end
