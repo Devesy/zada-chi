535bb3f9dde6adeea92af25b21799e8ac41af4ac
Anton Kalinin <trakses@gmail.com>
2024-06-07 01:02:38 +0400

    Update solve.rb
    https://leetcode.com/problems/ransom-note/

--- solve.rb
+++ solve.rb
@@ -1,1 +1,21 @@
+# @param {String} ransom_note
+# @param {String} magazine
+# @return {Boolean}
+def can_construct(ransom_note, magazine)
+    resource = Hash.new(0)
+    magazine.chars.each { |m| resource[m] += 1 }
 
+    demand = Hash.new(0)
+    ransom_note.chars.each { |n| demand[n] += 1 }
+    return false if demand.each_pair.any? { |k, v| res = resource[k]; res.nil? || v > res }
+    return true
+
+     # longer?
+    ransom_note.chars.each do |n|
+        current = resource[n]
+        return false if current.nil? || current == 0
+
+        resource[n] = current - 1
+    end
+    true
+end
