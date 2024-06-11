c438a695658fe03e187466fe1a9382fd39059f6d
Anton Kalinin <trakses@gmail.com>
2024-06-11 18:40:57 +0400

    Update solve.rb
    https://leetcode.com/problems/two-sum/description/

--- solve.rb
+++ solve.rb
@@ -1,1 +1,11 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    store = {}
+    nums.each_with_index do |n, i|
+        return [i, store[target - n]] if store[target - n] != nil
 
+        store[n] = i
+    end
+end
